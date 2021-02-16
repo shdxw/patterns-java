d8988f7adeb93b8facadf64fae59956074f1c0f7
vlader <Vladarscb@gmail.com>
2021-02-16 20:19:17 +1000

    добавлен паттерн синглтон

---
+++ src/main/java/ru/patterns/create/singleton/Singleton.java
@@ -0,0 +1,30 @@
+package ru.patterns.create.singleton;
+
+public final class Singleton { //нельзя наследоваться
+    private static Singleton instance; //статическое поле для инстанса
+    public String value; //значение, для иницилизации синглтона
+
+    private Singleton(String value) {
+        // Этот код эмулирует медленную инициализацию.
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance("FOO");
+        Singleton anotherSingleton = Singleton.getInstance("BAR");
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
+    }
+}
